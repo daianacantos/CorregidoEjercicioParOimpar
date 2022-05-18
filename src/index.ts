e4c23ffe21f91edf7331d0464746464a90482c5a
daianacantos <104039777+daianacantos@users.noreply.github.com>
2022-05-18 18:25:34 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/*Realizar un algoritmo que dado un
+número entero ingresado por el
+usuario, visualice en pantalla si es
+par o impar
+• En el caso de ingresar un cero, se
+debe volver a pedir el número por
+teclado (hasta que se ingrese un
+número mayor que cero)*/
+
+let numero: number = Number(prompt("ingrese un numero entero"));
+
+while (numero === 0) {
+  numero = Number(prompt("ingrese un nùmero mayor a cero"));
+}
+if (numero % 2 === 0) {
+  console.log("el número es PAR");
+} else {
+  console.log("el número es IMPAR");
+}
